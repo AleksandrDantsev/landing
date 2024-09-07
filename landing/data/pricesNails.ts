a3f930e31edcd41610729084c569e02ecc720320
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-09-07 16:39:05 +0300

    add prices

---
+++ landing/data/pricesNails.ts
@@ -0,0 +1,37 @@
+export const pricesNails = [
+    {
+        name: "Russian Style Manicure",
+        explanation: "Extended Wear. Reduced Hangnails. In-Depth Cuticle Removal.",
+        cost: ["$69", "$69*"],
+    },
+    {
+        name: "Cure Manicure",
+        explanation: "Choice of Scrub, Mask, and Moisturizer",
+        cost: ["$40", "$55*"],
+    },
+    {
+        name: "Classic Manicure",
+        explanation: "Choice of scrub OR mask and moisturizer",
+        cost: ["$35", "$50*"],
+    },
+    {
+        name: "Quickie Manicure",
+        explanation: "Choice of Moisturizer",
+        cost: ["$30", "$45*"],
+    },
+    {
+        name: "Cure Pedicure",
+        explanation: "Choice of Scrub, Mask, and Moisturizer",
+        cost: ["$45", "$60*"],
+    },
+    {
+        name: "Classic Pedicure",
+        explanation: "Choice of scrub OR mask and moisturizer",
+        cost: ["$40", "$55*"],
+    },
+    {
+        name: "Quickie Pedicure",
+        explanation: "Choice of soak and moisturizer",
+        cost: ["$35", "$50*"],
+    },
+]
