0c5d7fb53f6a57355d514bf8423971d8ed29e78f
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-09-07 16:40:42 +0300

    add data SingleCard

---
+++ landing/data/singleCardData.ts
@@ -0,0 +1,28 @@
+const singleMainData = {
+    backgroundClass: "light-brown-back",
+    mainImage: "/img-life-is-too.webp",
+    stainsIMG: [null, "/red.png", "/grey-dots-big.png", null],  // max number - 4
+    titles: ["Life is too short", "for unpolished nails."],  // max number - 2
+    textContent: "Our products focus on preventing aging in the places that matter most: hands & feet.",
+};
+
+
+const singleNailsData = {
+    backgroundClass: "gray-back",
+    mainImage: ["/img-step-1.webp", "/img-step-3-1.webp", "/img-section-1-2.webp"],
+    stainsIMG: ["/green-dots-top.png", "/blue-dots-right.png", null,  null],  // max number - 4
+    titles: ["Choose your", "style"],  // max number - 2
+    textContent: "Choice of soak, scrub or mask, and moisturizer",
+};
+
+const singleFacialData = {
+    backgroundClass: "gray-back",
+    mainImage: "/Img_face_by_cure.webp",
+    stainsIMG: ["/red.png", null, "/grey-dots-big.png", "/dots.png"],  // max number - 4
+    titles: ["Face", "by Cure"],  // max number - 2
+    textContent: null,
+};
+
+
+
+export { singleMainData, singleNailsData, singleFacialData };
