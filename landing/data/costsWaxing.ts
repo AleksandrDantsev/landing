becb0edeab656741192e0b7953004cd935adf866
Alexander Dantsev <ivan_sidorov_99@internet.ru>
2024-09-07 16:38:52 +0300

    add data cost table

---
+++ landing/data/costsWaxing.ts
@@ -0,0 +1,64 @@
+export const pricesWaxProcedure = [
+    {
+        title: "Head", 
+        procedures: [
+            [ "Sideburns", "$12" ],
+            [ "Nostrils", "$12" ]  ,
+            [ "Lip", "$12" ]  ,
+            [ "Chin", "$12" ]  ,
+            [ "Cheeks", "$12"] ,
+            [ "Ears", "$12" ],
+            [ "Neck", "$15" ],
+            [ "Eyebrows", "$22" ],
+            [ "Full Face", "$59" ],
+        ]
+    },
+    {
+        title: "Chest", 
+        procedures: [
+            [ "Underarms", "$22" ],
+            [ "Chest", "$23", "Partial" ],
+            [ "Chest", "$34", "Full" ],
+            [ "Stomach", "$17", "Partial" ],
+            [ "Stomach", "$34", "Full" ],
+        ]
+    },
+    {
+        title: "Back", 
+        procedures: [
+            [ "Back", "$27", "Upper" ],
+            [ "Back", "$27", "Middle" ]  ,
+            [ "Back", "$21", "Lower" ]  ,
+            [ "Back", "$66", "Full" ]  ,
+        ]
+    },
+    {
+        title: "Arms", 
+        procedures: [
+            [ "Shoulders", "$24"],
+            [ "Arms", "$36", "Half"],
+            [ "Arms", "$46", "Full"],
+            [ "Hands", "$15"],
+        ]
+    },
+    {
+        title: "The Sweet Spot", 
+        procedures: [
+            [ "The Peach", "$24", "Inner" ],
+            [ "The Peach", "$24", "Outer" ],
+            [ "The Peach", "$36", "Full" ],
+            [ "The Pear", "$36", "Our Bikini" ],
+            [ "Cherry", "$39"],
+            [ "Peach & Cherry", "$57", "Our Brazilian" ],
+        ]
+    },
+    {
+        title: "Legs", 
+        procedures: [
+            [ "Legs", "$44", "Lower" ],
+            [ "Legs", "$46", "Upper" ],
+            [ "Legs", "$82", "Full" ],
+            [ "Feet", "$15" ],
+        ]
+    },
+];
